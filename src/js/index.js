305240805ea41046524023f0bea38c235488e1a6
camile <camile@mbcases.com.br>
2024-12-10 15:11:58 -0300

    Carrossel

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,24 @@
 var doubtElements = document.querySelectorAll(".duvida");
 
-doubtElements.forEach(function(duvida){
+doubtElements.forEach(function(duvida) {
     duvida.addEventListener('click', function() {
-        duvida.classList.toggle('ativa')
-    })
+        duvida.classList.toggle('ativa');
+    });
 });
+
+const imagesCarrossel = document.getElementById("img-div");
+const images = document.querySelectorAll("#img-div .img-e");
+
+let idx = 0;
+
+function carrossel() {
+    idx++;
+
+    if (idx > images.length -1) {
+        idx = 0;
+    }
+
+    imagesCarrossel.style.transform = `translateX(${-idx * 580}px)`;
+}
+
+setInterval(carrossel, 1800);
